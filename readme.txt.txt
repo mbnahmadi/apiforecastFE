69336da078635f543e713f9b130c2d817adfcb9c
mbnahmadi <mbn.ahmadi78@gmail.com>
2022-11-20 13:40:54 +0330

    wave and wind forecast

---
+++ readme.txt.txt
@@ -0,0 +1,11 @@
+///To run this program, you need to run the apiforecastBE program in advance///
+1.Run the following command in the direction where manage.py exists.
+2.pip install -r req.txt (pip install requests)
+3.python manage.py runserver
+4.in your browser search http://127.0.0.1:8000/table/{file name}
+//notice:file name can be 'a','b','c','d'.
+//this tabe is for wind forecast
+5.in your browser search http://127.0.0.1:8000/waveforecast/{file name}
+//notice:file name can be 'o_IO101','o_IO102','o_IO103','o_IO104','o_IO105',
+'o_IO106','o_IO107','o_IO108','o_IO109'.
+//this tabe is for wave forecast
---
+++ web/views.py
@@ -0,0 +1,187 @@
+import json
+# from multiprocessing import context
+from urllib import response
+from django.shortcuts import render
+# from pyparsing import line_end
+import requests
+from django.http import HttpResponse
+# import pandas as pd
+# import csv
+import datetime
+# import json
+# Create your views here.
+
+
+
+
+def show(request):
+    response = requests.get('http://127.0.0.1:5000/api/a')
+    r = response.json()
+    context = {"api" : r,
+               }
+    # html = '<html><body>%s</body></html>' %r
+    return render(request , "test.html" , context)
+
+
+
+def source(request):
+    # response = requests.get('http://127.0.0.1:8001/api/0')
+    # r = response.json()
+    # context = {"api" : r,
+    #            }
+    # # html = '<html><body>%s</body></html>' %r
+    return render(request , "source.html" , {})
+    
+
+def TableView(request, name):
+    response = requests.get(f'http://127.0.0.1:5000/api/{name}')
+    r = response.json()
+    # html = '<html><body>%s</body></html>' %r
+    keys = r[0].keys()
+    val = []
+    for i, item in enumerate(r):
+        val.append([])
+        for key in keys:
+            val[i].append(item[key])
+    # print(val)
+    
+    #chart
+    list_a = []
+    list_b = []
+    # list_c = []
+    for i in r:
+        list_a.append(i['v4'])
+        list_b.append(i['v5'])
+        # list_c.append(i['v7'])
+        
+    #counting hours of the day   
+    start = datetime.time(0,0) # 10:00
+    end = datetime.time(23,0) # 10:05
+    TIME_FORMAT = "%H:%M" # Format for hours and minutes
+    time = [] # List of times 
+    while start <= end:
+        time.append(start)
+        if start.hour == 23:
+            break
+        start = start.replace(hour=start.hour + 1)
+    time = [x.strftime(TIME_FORMAT) for x in time] 
+    times = time * 10
+    context={
+        "api" : r,
+        "keys": keys,
+        "val": val,
+        "v4" : list_a ,
+        "v5" : list_b ,
+        # "v7" : list_c ,
+        "times" : times,
+    }
+    return render(request, "table.html", context)
+
+
+def ListView(request):
+    list_api = requests.get('http://127.0.0.1:5000/api/')
+    list_api = list_api.json()
+    lists = []
+    for i in list_api:
+        lists.append(i.removesuffix('.mean') )
+    context={
+        "list_api": lists,
+    }
+    return render(request, "list.html", context)
+
+
+# def Chart(request, name):
+#     response = requests.get(f'http://127.0.0.1:5000/api/{name}')
+#     r = response.json()
+#     # a = []
+#     # b = []
+#     # c = []
+#     list_a = []
+#     list_b = []
+#     list_c = []
+#     for i in r:
+#         list_a.append(i['v5'])
+#         list_b.append(i['v6'])
+#         list_c.append(i['v7'])
+#     start = datetime.time(0,0) # 10:00
+#     end = datetime.time(23,0) # 10:05
+#     TIME_FORMAT = "%H:%M" # Format for hours and minutes
+#     time = [] # List of times 
+#     while start <= end:
+#         time.append(start)
+#         if start.hour == 23:
+#             break
+#         start = start.replace(hour=start.hour + 1)
+#     time = [x.strftime(TIME_FORMAT) for x in time] 
+#     times = time * 10
+    
+#     #print(times)
+#     # print(times)   
+#     # print (time)
+#     context = {
+#         "api" : r , 
+#         "v5" : list_a ,
+#         "v6" : list_b ,
+#         "v7" : list_c ,
+#         "times" : times,
+        
+#     }
+
+#     return render(request , "source.html" , context)
+
+
+def CSVTableView(request, name):
+    response = requests.get(f'http://127.0.0.1:5000/api/waveforecast/{name}')
+    r = response.json()
+    # html = '<html><body>%s</body></html>' %r
+    keys = r[0].keys()
+    csvval = []
+    for i, item in enumerate(r):
+        csvval.append([])
+        for key in keys:
+            csvval[i].append(item[key])
+    # print(val)
+    
+    # #chart
+    list_a = []
+    list_b = []
+    list_c = []
+    list_d = []
+    for i in r:
+        list_a.append(i['a2'])
+        list_b.append(i['a3'])
+        list_c.append(i['a4'])
+        list_d.append(i['a5'])
+        
+    #counting hours of the day   
+    start = datetime.time(0,0) # 10:00
+    end = datetime.time(23,0) # 10:05
+    TIME_FORMAT = "%H:%M" # Format for hours and minutes
+    time = [] # List of times 
+    while start <= end:
+        time.append(start)
+        if start.hour == 23:
+            break
+        start = start.replace(hour=start.hour + 1)
+    time = [x.strftime(TIME_FORMAT) for x in time] 
+    times = time * 6
+    context={
+        "api" : r,
+        "keys": keys,
+        "csvval": csvval,
+        "a2" : list_a ,
+        "a3" : list_b ,
+        "a4" : list_c ,
+        "a5" : list_d,
+        "times" : times,
+    }
+    return render(request, "csvtable.html", context)
+
+
+
+
+
+
+
+
+
